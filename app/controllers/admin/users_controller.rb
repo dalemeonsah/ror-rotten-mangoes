de0cde12858f7be4daa2d589b757bd2f51d5cefb
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-03-03 05:22:52 +0000

    create/edit users(admins)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,5 +5,29 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all.page(params[:page]).per(2)
   end
+
+  def new
+    @user = User.new 
+    render '/users/new'
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
   
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
 end
