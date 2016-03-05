8c5a7358ced15ee5e508dc83e367e296d0356dc4
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-03-05 01:33:21 +0000

    AR scopes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -23,6 +23,20 @@ class Movie < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  scope :search_title, ->(title) {where("title like ?", "%#{title}%")}
+  scope :search_director, ->(director) {where("director like ?", "%#{director}%")}
+  scope :search_runtime, ->(runtime) {
+    case runtime
+    when "Under 90 minutes"
+      where("runtime_in_minutes < ?", 90) 
+    when "Between 90 and 120 minutes"
+      where("runtime_in_minutes BETWEEN ? AND ?", 90, 120) 
+    when "Over 120 minutes"
+      where("runtime_in_minutes > ?", 120) 
+    end
+  }
+
+
   def review_average
     reviews.size == 0 ? @errors = "unavailable" : reviews.sum(:rating_out_of_ten)/reviews.size
   end
